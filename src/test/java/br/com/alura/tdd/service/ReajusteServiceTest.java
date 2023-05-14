5141bb0760686fc7a7ebc9f5416adc0b1e923150
Gustavo Felipe Scheneider <gusfelipes@gmail.com>
2023-05-13 23:24:04 -0300

    chore: atedendo aos regras de bom e otimo desempenho

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -22,5 +22,27 @@ public class ReajusteServiceTest {
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 
 	}
+	
+	@Test
+	public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom() {
+		ReajusteService service = new ReajusteService();
+		Funcionario funcionario = new Funcionario("Gustavo", LocalDate.now(), new BigDecimal("1000"));
+
+		service.concederReajuste(funcionario, Desempenho.BOM);
+
+		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
+
+	}
+	
+	@Test
+	public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo() {
+		ReajusteService service = new ReajusteService();
+		Funcionario funcionario = new Funcionario("Gustavo", LocalDate.now(), new BigDecimal("1000"));
+
+		service.concederReajuste(funcionario, Desempenho.OTIMO);
+
+		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
+
+	}
 
 }
